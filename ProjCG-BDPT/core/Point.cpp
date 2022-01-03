89361038543c99d2d973308a5d05eaec6497617f
Luis <lcld@cin.ufpe.br>
2022-01-03 13:39:04 -0300

    Envio dos arquivos que faltaram

---
+++ ProjCG-BDPT/core/Point.cpp
@@ -0,0 +1,12 @@
+#include "Point.h"
+Point::Point(float x, float y, float z){
+		this->x = x;
+		this->y = y;
+		this->z = z;
+}
+
+Point::Point(){
+		this->x = 0;
+		this->y = 0;
+		this->z = 0;
+}
